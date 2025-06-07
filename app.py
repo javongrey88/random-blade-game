38274670ce2115b98ed67fb55cb7aad27b630336
javongrey707 <javongrey707@gmail.com>
2025-06-06 21:55:50 -0600

    Phase 1: Initialize repo, add Flask skeleton and requirements

---
+++ app.py
@@ -0,0 +1,11 @@
+# app.py
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return "<h1>Random Blade: Flask App is Running!</h1>"
+
+if __name__ == "__main__":
+    app.run(debug=True)
